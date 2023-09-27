ba777c0ac5bd71f69969bcf97d2f45638eb0f066
tanvir8321 <tanvir8321@gmail.com>
2023-09-27 22:59:05 +0600

    live db added

--- src/server.ts
+++ src/server.ts
@@ -6,24 +6,24 @@ import sequelize from './sequelizeConfig';
 const app = express();
 const port = 3000;
 
-// Sync Sequelize with the database
-sequelize
-  .sync()
-  .then(() => {
-    console.log('Database synced');
-  })
-  .catch((error) => {
-    console.error('Error syncing database:', error);
-  });
+// // Sync Sequelize with the database
+// sequelize
+//   .sync()
+//   .then(() => {
+//     console.log('Database synced');
+//   })
+//   .catch((error) => {
+//     console.error('Error syncing database:', error);
+//   });
 
-app.get('/', async (req, res) => {
-  try {
-    res.send(`Hello, Tanvir`);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// app.get('/', async (req, res) => {
+//   try {
+//     res.send(`Hello, Tanvir`);
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
